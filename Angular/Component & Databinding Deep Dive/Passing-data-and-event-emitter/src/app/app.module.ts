4a6d91733be28b859e320228fb0544ae06ed61d6
MS <msyukron10@gmail.com>
2023-12-29 14:12:29 +0700

    -

--- Angular/Component & Databinding Deep Dive/Passing-data-and-event-emitter/src/app/app.module.ts
+++ Angular/Component & Databinding Deep Dive/Passing-data-and-event-emitter/src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { TableComponent } from './table/table.component';
 import { FormsModule } from '@angular/forms';
-import { ExampleComponent } from './example/example.component';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
-    TableComponent,
-    ExampleComponent,
-    CockpitComponent,
-    ServerElementComponent
+    TableComponent
   ],
   imports: [
     BrowserModule, FormsModule
